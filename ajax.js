2446e3f939e3315f31e0e8efdad57982e03e1e23
a-serdyuk@bitbucket.org <a-serdyuk@bitbucket.org>
2023-08-21 21:42:05 +0530

    apply promises

--- ajax.js
+++ ajax.js
@@ -7,10 +7,11 @@
 
 const
   appendPath = (path, data) => {
+    const pathArr = []
     for (let key in data) {
-      path += "&" + key + "=" + data[key];
+      pathArr.push(`${key}=${data[key]}`);
     }
-    return path;
+    return path + '?' + pathArr.join('&');
   },
 
   createRequest = () => {
@@ -19,11 +20,10 @@ const
     } else if (window.ActiveXObject) {
       let xhr;
       try {
-        xhr = new ActiveXObject("Msxml2.XMLHTTP");
-      } catch (e) {
-      }
+        xhr = new ActiveXObject('Msxml2.XMLHTTP');
+      } catch (e) {}
       try {
-        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        xhr = new ActiveXObject('Microsoft.XMLHTTP');
       } catch (e) {
         return null;
       }
@@ -33,111 +33,117 @@ const
   },
   /**
    * @param {array} params
-   * @return {void}
+   * @return {Promise}
    */
   sendRequest = (params) => {
-    const xhr = createRequest();
-    if (!xhr) {
-      alert("Browser does not support ajax");
-      return;
-    }
-    let
-      sendData,
-      path = params["path"] + "?ajax=true",
-      callback = params.callback,
-      requestType = params["type"],
-      data = params["data"] || {},
-      respType = params["respType"] || "json",
-      contentType = params["contentType"] || "application/x-www-form-urlencoded"
-    ;
-
-    if (requestType === "GET") {
-      path = appendPath(path, data);
-    } else if (contentType === "multipart/form-data") {
-      let boundary = String(Math.random()).slice(2);
-      contentType += '; boundary=' + boundary;
-      sendData = '\r\n--' + boundary + '\r\nContent-Disposition: form-data; name="data"; filename="' + data.fileName + '"\r\nContent-Type: ' + data.fileType + '\r\n\r\n' + data.data + '\r\n--' + boundary + '--\r\n';
-      delete data.fileName;
-      delete data.fileType;
-      delete data.data;
-      path = appendPath(path, data);
-    } else {
-      sendData = [];
-      for (let key in data) {
-        sendData.push(key + "=" + data[key]);
+    return new Promise(function (resolve, reject) {
+      const xhr = createRequest();
+      if (!xhr) {
+        alert('Browser doesn`t support ajax');
+        return;
       }
-      sendData = sendData.join("&");
-    }
-    xhr.open(requestType, path, true);
-    xhr.setRequestHeader("Content-Type", contentType);
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          let rData = xhr.responseText;
-          if (respType === "json") {
-            if (rData === "true") {
-              return { success: true };
-            }
-            if (rData === "false") {
-              return { success: false };
-            }
-            const eData = !(/[^,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]/.test(rData.replace(/"(\\.|[^"\\])*"/g, ""))) && eval("(" + rData + ")");
-            const eArray = new Object(eData);
-            callback(eArray);
-          } else {
-            callback(rData);
-          }
+      let
+        sendData,
+        path = params.path,
+        requestType = params.type,
+        data = params.data || {},
+        respType = params.respType || 'json',
+        contentType = params.contentType || 'application/x-www-form-urlencoded'
+      ;
+      if (requestType === 'GET') {
+        path = appendPath(path, data);
+      } else if (contentType === 'multipart/form-data') {
+        let boundary = String(Math.random()).slice(2);
+        contentType += '; boundary=' + boundary;
+        sendData = '\r\n--' + boundary + '\r\nContent-Disposition: form-data; name="data"; filename="' + data.fileName + '"\r\nContent-Type: ' + data.fileType + '\r\n\r\n' + data.data + '\r\n--' + boundary + '--\r\n';
+        delete data.fileName;
+        delete data.fileType;
+        delete data.data;
+        path = appendPath(path, data);
+      } else {
+        sendData = [];
+        for (let key in data) {
+          sendData.push(key + '=' + data[key]);
         }
+        sendData = sendData.join('&');
       }
-    };
-    // The request body is ready, send it
-    xhr.send(sendData);
-  }
-;
+      xhr.open(requestType, path, true);
+      xhr.setRequestHeader('Content-Type', contentType);
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          let respData = xhr.responseText;
+          if (respType === 'json') {
+            if (respData === 'true') {
+              resolve({ success: true });
+            }
+            if (respData === 'false') {
+              resolve({ success: false });
+            }
+            const eData = !(/[^,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]/.test(respData.replace(/"(\\.|[^"\\])*"/g, ''))) && eval('(' + respData + ')');
+            resolve(new Object(eData));
+          } else {
+            resolve(respData);
+          }
+        } else {
+          reject(new Error(`Request error`))
+        }
+      };
+      // The request body is ready, send it
+      xhr.send(sendData);
+    });
+  };
+
 const ajax = {
   /**
    * Sending a get request
    *
    * @param {string} path
-   * @param {mixed} data параметры запроса
-   * @param {function} callback
+   * @param {mixed} data request parameters
    * @param {string} respType
    * @param {string} contentType
-   * @return {void}
+   * @return {Promise}
    */
-  get: (path, data, callback, respType, contentType) => {
-    if (typeof data === "function") {
-      respType = callback;
-      callback = data;
-      data = {};
-    }
-    sendRequest({
-      type: "GET",
-      path: path,
-      data: data,
-      callback: callback,
-      respType: respType,
-      contentType: contentType,
+  get: (path, data, respType, contentType) => {
+    return new Promise(function (resolve, reject) {
+      sendRequest({
+        type: 'GET',
+        path: path,
+        data: data,
+        respType: respType,
+        contentType: contentType,
+      })
+        .then(result => {
+          resolve(result)
+        })
+        .catch(e => {
+          reject(e);
+        });
     });
   },
   /**
    * Sending a post request
    *
    * @param {string} path
-   * @param {mixed} data параметры запроса
-   * @param {function} callback
+   * @param {mixed} data request parameters
    * @param {string} respType
    * @param {string} contentType
-   * @return {void}
+   * @return {Promise}
    */
-  post: (path, data, callback, respType, contentType) => {
-    sendRequest({
-      type: "POST",
-      path: path,
-      data: data,
-      callback: callback,
-      respType: respType,
-      contentType: contentType,
+  post: (path, data, respType, contentType) => {
+    return new Promise(function (resolve, reject) {
+      sendRequest({
+        type: 'POST',
+        path: path,
+        data: data,
+        respType: respType,
+        contentType: contentType,
+      })
+        .then(result => {
+          resolve(result)
+        })
+        .catch(e => {
+          reject(e);
+        });
     });
   },
 };
